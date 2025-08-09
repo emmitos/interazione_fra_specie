60444e6ae5b3e48e495af6f69ccd5228fc45bb42
Emma Carloni <emma@MacBook-di-Emma.local>
2025-08-09 11:52:24 +0200

    setup iniziale

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+    /* quando fai i nuovi file.hpp e .cpp ricordati di modificare il CMakeLists.txt(in realtà solo coin i .cpp tanto gli altri sono inclusi
+    ), poi al momento del test aggiungilo di nuovo nel cmake. 
+    PER COMPILARE : se hai modificato il CMakeLists allora fai : build ..
+    poi SEMPRE devi fare  : cmake --build . ( per compilare) 
+    (per eseguire) : ./interazione_fra_specie*/
+}
